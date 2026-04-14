640cd913ebe341552a8995f351e7e35b1efc698d
chantchan <64460401+chantchan@users.noreply.github.com>
2026-04-15 00:16:33 +0800

    Create database configuration file for MySQL connection pool using mysql2/promise

---
+++ config/database.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const mysql = require('mysql2/promise');
+
+const pool = mysql.createPool({
+    host: 'localhost',
+    user: 'your_username',
+    password: 'your_password',
+    database: 'your_database',
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
+});
+
+module.exports = pool;
